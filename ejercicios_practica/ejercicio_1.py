7c2b94e99b3d41a051a099141cbb461de6f8ddb3
ripol09 <101203150+ripol09@users.noreply.github.com>
2022-03-19 19:48:07 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -9,8 +9,8 @@
 
 # Ejercicios de práctica numérica
 
-numero_1 = 5
-numero_2 = 7
+numero_1 = 6
+numero_2 = 9
 
 # Realizar la suma de las dos variables
 # numero_1 y numero_2
@@ -22,3 +22,10 @@ numero_2 = 7
 # print(....)
 
 # Repita el procedimiento para realizar la resta
+suma= numero_1 + numero_2
+
+print (suma)
+
+resta= numero_1 - numero_2
+
+print (resta)
